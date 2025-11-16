ad8390f4e788c99faf8c5323de89e43ce7a534dd
James Sizeland <jamessizeland@gmail.com>
2025-11-16 10:50:58 +0000

    add documentation

--- src/room.rs
+++ src/room.rs
@@ -1,3 +1,5 @@
+//! Game Room
+
 mod actions;
 mod chat;
 mod events;
@@ -10,13 +12,32 @@ use iroh_docs::{AuthorId, api::Doc};
 use std::sync::Arc;
 
 pub use events::GameEvent;
+pub use setup::{RoomBuilder, RoomEntry};
 
 #[derive(Clone)]
 pub struct GameRoom<G: GameLogic> {
+    /// Networking interface
     pub(self) iroh: Iroh,
+    /// Persistent data store
     pub(self) doc: Doc,
+    /// Document writer unique identifier
     pub(self) author: AuthorId,
+    /// Game logic
     pub(self) logic: Arc<G>,
-    pub is_host: bool,
-    pub id: EndpointId,
+    /// Whether we are the host
+    is_host: bool,
+    /// Our location unique identifier
+    id: EndpointId,
+}
+
+impl<G: GameLogic> GameRoom<G> {
+    /// Get Iroh Network Endpoint ID
+    pub fn id(&self) -> EndpointId {
+        self.id
+    }
+    /// Is this gameroom instance hosting?
+    pub fn is_host(&self) -> bool {
+        // TODO we should be checking this from the document not from our local state, incase the host has migrated.
+        self.is_host
+    }
 }
